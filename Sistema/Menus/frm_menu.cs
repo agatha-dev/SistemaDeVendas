a09ed5894e559eca3dcdad9d07ed38c17f807983
agatha <agathasantos145@gmail.com>
2021-10-12 01:45:49 -0300

    Passando os parametros para receber as janelas

---
+++ Sistema/Menus/frm_menu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema
+{
+    public partial class frm_menu : Form
+    {
+        public frm_menu()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Produtos_Click(object sender, EventArgs e)
+        {
+            frm_Produtos frm = new frm_Produtos();
+            frm.Show();
+        }
+
+        private void Usuário(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_categorias_Click(object sender, EventArgs e)
+        {
+            frm_Categorias Frm = new frm_Categorias();
+            Frm.Show();
+        }
+
+        private void produtosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Produtos frm = new frm_Produtos();
+            frm.Show();
+        }
+
+        private void categoriasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Categorias Frm = new frm_Categorias();
+            Frm.Show();
+        }
+    }
+}
